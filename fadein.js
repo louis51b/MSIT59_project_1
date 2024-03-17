2f14f2df7bc4abcf31319e5dc465d625e50201b0
LOUIS6371 <cheng2louis@gmail.com>
2024-03-17 22:08:14 +0800

    首頁+關卡攻略+最新資訊+貓咪圖鑑進度完成

--- fadein.js
+++ fadein.js
@@ -4,9 +4,9 @@ function fadeIn(elementId, delay) {
       }, delay);
     }
     fadeIn('object1', 500); // 延遲 500 毫秒後 object1 出現
-    fadeIn('object2', 1000); // 延遲 1500 毫秒後 object2 出現
-    fadeIn('object3', 1500); // 延遲 2500 毫秒後 object3 出現
-    fadeIn('object4', 2000);
+    fadeIn('object2', 750); // 延遲 1500 毫秒後 object2 出現
+    fadeIn('object3', 1000); // 延遲 2500 毫秒後 object3 出現
+    fadeIn('object4', 1250);
     fadeIn('bg-img-1', 500);
     fadeIn('bg-img-2', 500);
     fadeIn('bg-img-3', 500);
